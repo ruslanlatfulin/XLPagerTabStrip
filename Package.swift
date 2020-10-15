8655048ab4d5b9241b0d47c47e82899ae9d79dd4
Ruslan Latfulin <latfulinr@gmail.com>
2020-10-15 17:39:50 +0300

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "XLPagerTabStrip",
             dependencies: [],
-            sources: ["Sources"]
+            resources: [.process("ButtonCell.xib")]
         ),
         .testTarget(
             name: "XLPagerTabStripTests",
