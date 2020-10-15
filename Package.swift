6a841cf771b0eb4fd3fce450918e73c7c823cf72
Ruslan Latfulin <latfulinr@gmail.com>
2020-10-15 17:39:18 +0300

    FIx to SPM

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "XLPagerTabStrip",
-            dependencies: [], resources: [.copy("ButtonCell.xib")]),
+            dependencies: [],
+            sources: ["Sources"]
+        ),
         .testTarget(
             name: "XLPagerTabStripTests",
             dependencies: ["XLPagerTabStrip"]),
